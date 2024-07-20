184b7393e3fd2ae07fa974b888ed9f90f0a4ff40
Josue Vargas <josuevargass@hotmail.com>
2024-07-20 08:57:56 -0500

    PRIMER CAMBIO
    PRIMER CAMBIO

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Storage } from '@ionic/storage-angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,38 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  newTodo: string = '';
+  todos: string[] = [];
 
-  constructor() {}
+  constructor(private storage: Storage) {
+    this.storage.create();
+    this.getItem();
+  }
 
+  setItem() {
+    localStorage.setItem('proyectos', JSON.stringify(this.todos));
+  }
+
+  getItem() {
+    var proyectos = localStorage.getItem('proyectos');
+
+    if (proyectos == null) return;
+
+    this.todos = JSON.parse(proyectos!);
+    console.log(this.todos);
+  }
+
+  addTodo() {
+    if (this.newTodo.trim().length > 0) {
+      this.todos.push(this.newTodo);
+      this.newTodo = '';
+      this.setItem();
+      this.getItem();
+    }
+  }
+
+  removeTodo(index: number) {
+    this.todos.splice(index, 1);
+    this.setItem();
+  }
 }
